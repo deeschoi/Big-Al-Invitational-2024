d7cea29c049785d5e5057140f04d7c90896d77a6
Daniel C <danielschoi.124@gmail.com>
2025-01-28 02:13:56 -0500

    Update CREATE TABLE results.sql

--- CREATE TABLE results.sql
+++ CREATE TABLE results.sql
@@ -1,10 +1,10 @@
 CREATE TABLE results (
-	EventNum	INT				NOT NULL,
+    EventNum	INT		NOT NULL,
     Placement	INT(50),
-    Final		VARCHAR(100)	NOT NULL,
-    PreTime		VARCHAR(100)	NOT NULL,
+    Final	VARCHAR(100)	NOT NULL,
+    PreTime	VARCHAR(100)	NOT NULL,
     FinalTime	VARCHAR(100)	NOT NULL,
-    SwimcloudID	INT(50)			NOT NULL,
+    SwimcloudID	INT(50)		NOT NULL,
     FOREIGN KEY (SwimcloudID) REFERENCES swimmers(SwimcloudID),
     FOREIGN KEY (EventNum) REFERENCES records(EventNum)
 );
