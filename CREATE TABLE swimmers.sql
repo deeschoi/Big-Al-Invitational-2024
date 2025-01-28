8ecb4f5f9c16e5f8e23e87808889164f1e87f03d
Daniel C <danielschoi.124@gmail.com>
2025-01-28 02:14:37 -0500

    Update CREATE TABLE swimmers.sql

--- CREATE TABLE swimmers.sql
+++ CREATE TABLE swimmers.sql
@@ -1,15 +1,15 @@
 USE bigal;
 
 CREATE TABLE swimmers (
-	School		CHAR(100) NOT NULL,
-    SwimcloudID	INT(50) NOT NULL,
-    LastName	CHAR(100) NOT NULL,
-    FirstName	CHAR(100) NOT NULL,
-    SEX			CHAR(1),
-    Class		CHAR(2),
-    City		CHAR(100),
-    State		CHAR(10),
-    Country		CHAR(3),
+    School	CHAR(100)	NOT NULL,
+    SwimcloudID	INT(50)		NOT NULL,
+    LastName	CHAR(100)	NOT NULL,
+    FirstName	CHAR(100)	NOT NULL,
+    SEX		CHAR(1),
+    Class	CHAR(2),
+    City	CHAR(100),
+    State	CHAR(10),
+    Country	CHAR(3),
     PRIMARY KEY (SwimcloudID)
 );
 
