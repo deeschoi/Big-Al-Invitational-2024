9f3909c568e6695bf95fa562a72a5bb58232d318
Daniel C <danielschoi.124@gmail.com>
2025-01-28 02:16:24 -0500

    Update CREATE TABLE records.sql

--- CREATE TABLE records.sql
+++ CREATE TABLE records.sql
@@ -1,13 +1,13 @@
 USE bigal;
 
 CREATE TABLE records(
-    EventNum 		INT 	     NOT NULL,
-    EventName		VARCHAR(100) NOT NULL,
-    PoolRecord		VARCHAR(100) NOT NULL,
+    EventNum 		INT 	        NOT NULL,
+    EventName		VARCHAR(100)    NOT NULL,
+    PoolRecord		VARCHAR(100)    NOT NULL,
     RecordSchool	CHAR(100),
     RecordDate		INT,
-    D1A 		VARCHAR(100),
-    D1B 		VARCHAR(100),
+    D1A 		    VARCHAR(100),
+    D1B 		    VARCHAR(100),
     FOREIGN KEY (EventNum) REFERENCES EventList(EventNum)
 );
 
