c529608b8b7d2c7aaea66083553a6457ad189ae2
Daniel C <danielschoi.124@gmail.com>
2025-01-28 02:16:35 -0500

    Update CREATE TABLE records.sql

--- CREATE TABLE records.sql
+++ CREATE TABLE records.sql
@@ -6,8 +6,8 @@ CREATE TABLE records(
     PoolRecord		VARCHAR(100)    NOT NULL,
     RecordSchool	CHAR(100),
     RecordDate		INT,
-    D1A 		    VARCHAR(100),
-    D1B 		    VARCHAR(100),
+    D1A 		   VARCHAR(100),
+    D1B 		   VARCHAR(100),
     FOREIGN KEY (EventNum) REFERENCES EventList(EventNum)
 );
 
